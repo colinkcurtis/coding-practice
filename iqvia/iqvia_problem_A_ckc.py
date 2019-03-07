69128296c839b284c74ab0f239f9986ccf385fab
colinkcurtis <colinkcurtis@gmail.com>
2019-03-07 07:19:27 -0500

    made the code dynamic, improved naming, and reduced import footprint

--- iqvia/iqvia_problem_A_ckc.py
+++ iqvia/iqvia_problem_A_ckc.py
@@ -1,10 +1,13 @@
 # Usage: "$python3 iqvia_problemA_ckc.py <test_string>" where you replace <teststring> with any contiguous string you like.
 
-import itertools
-import sys
+from itertools import permutations
+from sys import argv
+from math import floor
 
-test_string = sys.argv[1]
+test_string = argv[1]
 two_string_set = set()
+test_string_length = len(test_string)
+loop_length = floor(test_string_length/2)+1
 
 def split_string(a_string):
     len_2_substrings =  [a_string[x:x+2] for x in range(0, len(a_string), 1) if len(a_string[x:x+2]) == 2]
@@ -12,23 +15,22 @@ def split_string(a_string):
     all_substrings = len_2_substrings + len_3_substrings
     set_of_substrings = set()
     forbidden_adjacent_identical_strings = False
-    for y in range(1,4):
-        all_combinations_gen = itertools.permutations(all_substrings, y)
-        for combination in all_combinations_gen:
-            for index, value in enumerate(combination[:-1]):
-                if value == combination[index+1]:
+    for y in range(1,loop_length):
+        all_permutations_gen = permutations(all_substrings, y)
+        for permutation in all_permutations_gen:
+            for index, value in enumerate(permutation[:-1]):
+                if value == permutation[index+1]:
                     forbidden_adjacent_identical_strings = False
                     break
                 else:
                     forbidden_adjacent_identical_strings = True
             if forbidden_adjacent_identical_strings == True:
-                new_string = ''.join(combination)
+                new_string = ''.join(permutation)
                 if new_string == a_string:
-                    for index, value in enumerate(combination):
+                    for index, value in enumerate(permutation):
                         set_of_substrings.add(value)
     list_of_substrings = list(set_of_substrings)
     list_of_substrings.sort(key=str.lower)
-    #print(set_of_substrings)
     print(list_of_substrings)
 
 if __name__ == '__main__':
